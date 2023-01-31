426bac98467c7fca12bf34f4a8a98773762fed8c
tkohldesac <topher@kohldesac.com>
2023-01-30 23:16:39 -0800

    Initializing

--- backend/routes/endpoints.js
+++ backend/routes/endpoints.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const knex = require('../knexConfig');
+const knex = require("../knexConfig");
 
-router.get('/something', async (req, res) => {
-  const dbResp = await knex.select('first_name').from('users');
-  console.log(dbResp)
+router.get("/something", async (req, res) => {
+  // const dbResp = await knex.select('first_name').from('users');
+  // console.log(dbResp)
   res.send(200);
 });
 
--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ExampleComponent from "./components/ExampleComponent.js";
+import Test from "./components/Test.js";
 
 export default function App() {
 
   return (
     <div>
-      <ExampleComponent/>
+      <Test />
+      <ExampleComponent />
     </div>
   );
 }
