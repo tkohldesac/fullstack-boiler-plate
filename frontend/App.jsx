5a70dd76ec0afde4a25a674b915b027fce8b6df9
tkohldesac <topher@kohldesac.com>
2023-02-01 23:15:01 -0800

    Messed around a LOT with the container. It still isn't working how I expect it to.

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ExampleComponent from "./components/ExampleComponent.js";
 import Test from "./components/Test.js";
+import Container from "@mui/material/Container";
 
 export default function App() {
 
   return (
     <div>
+      <Container fixed maxWidth="lg" bgcolor="green" sx={{ height: '98vh' }} >
       <Test />
       <ExampleComponent />
+      </Container>
+      
     </div>
   );
 }
--- frontend/components/Test.js
+++ frontend/components/Test.js
@@ -1,10 +1,29 @@
-import * as React from "react";
-import Button from "@mui/material/Button";
+import React, { useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
 
-export default function MyApp() {
+const StyledButton = styled.button`
+  width: 5rem;
+  height: 2rem;
+  outline: none;
+  border: none;
+  background-color: #c0c;
+  border-radius: 0.25rem;
+  );
+  transition: all 0.2s ease-out;
+`;
+
+export default function ExampleComponent() {
+  const [clickedCount, setClickedCount] = useState(0);
+
+  const onClick = async () => {
+    await axios.get("/something");
+    setClickedCount(clickedCount + 1);
+  };
+
   return (
-    <div>
-      <Button variant="contained">Hello World</Button>
-    </div>
+    <StyledButton clickedCount={clickedCount} onClick={onClick}>
+      Click me
+    </StyledButton>
   );
 }
